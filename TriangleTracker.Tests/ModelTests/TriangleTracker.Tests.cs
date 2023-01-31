9387baa01a94881b756c5292cebe6d8c0d20e2d1
ambi hidalgo <ambio.pk@gmail.com>
2023-01-30 19:48:47 -0800

    Test 2 isTriangle sides are positive int, pass
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Dani Steely <danisteelyart@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTracker.Tests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTracker.Tests.cs
@@ -12,5 +12,17 @@ namespace TriangleTracker.Tests
       Triangle testTriangle = new Triangle();
       Assert.AreEqual(true, testTriangle.IsTriangle(3,4,5));
     }
+    [TestMethod]
+    public void IsTriangle_AllSidesArePositive_True()
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.IsTriangle(3,4,5));
+    }
+    [TestMethod]
+    public void IsTriangle_AllSidesArePositive_False()
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(false, testTriangle.IsTriangle(-3,4,5));
+    }
   }
 }
