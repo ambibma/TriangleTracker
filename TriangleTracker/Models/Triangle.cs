4886075455e67b759465923d1219320307a6a308
ambi hidalgo <ambio.pk@gmail.com>
2023-01-30 20:39:23 -0800

    Test 5 TriangleType Scalene PASS
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Dani Steely <danisteelyart@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -12,6 +12,10 @@ namespace TriangleTracker
     /// <param><c>side1</c> is the first side.</param>
     /// <param><c>side2</c> is the second side.</param>
     /// <param><c>side3</c> is the third side.</param>
+    /// <returns>
+    /// True if the sides can form a triangle; otherwise,
+    /// false.
+    /// </returns>
     public bool IsTriangle(int side1, int side2, int side3)
     {
       if ((side1 < 0) || (side2 < 0) || (side3 < 0))
@@ -27,13 +31,31 @@ namespace TriangleTracker
         return true;
       }
     }
+    
+    /// <summary>
+    /// This method checks to see what type of triangle is
+    /// formed by the three input sides.
+    /// </summary>
+    /// <param><c>side1</c> is the first side.</param>
+    /// <param><c>side2</c> is the second side.</param>
+    /// <param><c>side3</c> is the third side.</param>
+    /// <returns>
+    /// A string describing the type of triangle.
+    /// </returns>
     public string TriangleType(int side1, int side2, int side3)
     {
       if (!IsTriangle(side1, side2, side3))
       {
         return "Hey, this ain't no got dang triangle";
       }
-      return "";
+      if ((side1 != side2) && (side1 != side3) && (side2 != side3))
+      {
+        return "Scalene";
+      } 
+      else
+      {
+        return "";
+      }
 
     }
   }
