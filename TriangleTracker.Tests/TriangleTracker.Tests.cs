90f670649e327667b301e4d678169076d4532a46
ambi hidalgo <ambio.pk@gmail.com>
2023-01-30 19:06:21 -0800

    Add triangle tracker test cs file
    Co-authored-by: Dani Steely <danisteelyart@gmail.com>
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>

---
+++ TriangleTracker.Tests/TriangleTracker.Tests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName;
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    // Test methods go here
+  }
+}
