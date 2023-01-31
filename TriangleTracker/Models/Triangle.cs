f3228c3593ede4defe37988d27da7312739a23a5
ambi hidalgo <ambio.pk@gmail.com>
2023-01-30 19:44:16 -0800

    Test 1 is triangle pass
    Co-authored-by: Dani Steely <danisteelyart@gmail.com>
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -1,10 +1,28 @@
 namespace TriangleTracker
 {
+  /// <summary>
+  /// Class <c>Triangle</c> models a two-dimensional triangle.
+  /// </summary>
   public class Triangle
   {
     public bool IsTriangle(int side1, int side2, int side3)
     {
-    return false;
+      if ((side1 < 0) || (side2 < 0) || (side3 < 0))
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+      // if ((side1 >= side2 + side3) || (side2 >= side1 + side3) || (side3 >= side2 + side1))
+      // {
+      //   return false;
+      // }
+      // else
+      // {
+      //   return true;
+      // }
     }
   }
 }
